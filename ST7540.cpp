e9ed45018101c8be368734fb62cdeb4d0889ca38
Ibrohim Kholilul Islam <ibrohimislam@gmail.com>
2017-06-28 07:58:09 +0700

    refactor ST7540 functions

--- ST7540.cpp
+++ ST7540.cpp
@@ -4,3 +4,69 @@
  * Created: 6/26/2017 4:23:44 PM
  * Author: @ibrohimislam
  */ 
+#include "ST7540.h"
+
+ISR(PCINT1_vect){
+	if ((PIN_CD_PD & (1<<CD_PD))==0) {
+		ss_clear();
+		}else{
+		ss_set();
+	}
+}
+
+struct bus_t initialize() {
+	
+	struct bus_t spi_bus;
+		
+	// set LED as output
+	DDR_LED |=  (1<<LED1) | (1<<LED2);
+		
+	// set SPI_SS as output
+	DDR_SS  |=  (1<<SS_PIN);
+		
+	spi_bus = spi_hw_poll_bus_get();
+	spi_hw_poll_init(E_SPI_MODE_SLAVE, E_SPI_SPEED_F2);
+	spi_hw_poll_clock_mode_set(E_CLOCK_MODE0);
+	
+	// ST7540 TX setup
+	ST7540_init();
+	ST7540_setup(spi_bus);
+	
+	PORT_LED |=  (1<<LED1);
+
+	data_acc();
+	rx_mode();
+	
+	return spi_bus;
+	
+}
+
+void ST7540_init(){
+	//Configure output
+	DDR_RXTX        |=  (1<<RXTX);
+	DDR_REG_DATA    |=  (1<<REG_DATA);
+	
+	//Configure input
+	DDR_CD_PD       &=  ~(1<<CD_PD);
+	DDR_BU_THERM    &=  ~(1<<BU_THERM);
+	
+	ss_set();
+}
+
+void ST7540_setup(struct bus_t spi_bus){
+	reg_acc();
+	tx_mode();
+
+	ss_clear();
+	spi_bus.f_send(spi_bus.priv, ST7540_REG, sizeof(ST7540_REG)/sizeof(*ST7540_REG), 0);
+	ss_set();
+}
+
+void ST7540_get_register(struct bus_t spi_bus, uint8_t* reg){
+	reg_acc();
+	rx_mode();
+	
+	ss_clear();
+	spi_bus.f_recv(spi_bus.priv, reg, 4, 0);
+	ss_set();
+}
--- ST7540.h
+++ ST7540.h
@@ -9,8 +9,63 @@
 #ifndef ST7540_H_
 #define ST7540_H_
 
+extern "C" {
+	#include "pca.h"
+	#include "bus.h"
+}
 
+#include <stdint.h>
+#include <avr/interrupt.h>
 
+#define DDR_BU_THERM    DDRC
+#define DDR_RXTX        DDRC
+#define DDR_REG_DATA    DDRC
+#define DDR_CD_PD       DDRC
 
+#define PORT_BU_THERM   PORTC
+#define PORT_RXTX       PORTC
+#define PORT_REG_DATA   PORTC
+#define PORT_CD_PD      PORTC
+
+#define PIN_BU_THERM    PINC
+#define PIN_CD_PD       PINC
+
+#define BU_THERM        PC0
+#define RXTX            PC1
+#define REG_DATA        PC4
+#define CD_PD           PC5
+
+#define reg_acc()       PORT_REG_DATA   |=  (1<<REG_DATA);
+#define data_acc()      PORT_REG_DATA   &=  ~(1<<REG_DATA);
+
+#define tx_mode()       PORT_RXTX   &=  ~(1<<RXTX);
+#define rx_mode()       PORT_RXTX   |=  (1<<RXTX);
+
+// SPI
+#define PORT_SPI        PORTB
+#define PIN_SPI         PINB
+#define DDR_SS          DDRB
+#define PORT_SS         PORTB
+#define ss_clear()      PORT_SS &=  ~(1<<SS_PIN);
+#define ss_set()        PORT_SS |=  (1<<SS_PIN);
+
+// LED
+#define SS_PIN          PB1
+#define DDR_LED         DDRD
+#define PORT_LED        PORTD
+#define LED1            PD6
+#define LED2            PD7
+
+extern uint8_t ST7540_REG[3];
+
+// private
+
+void ST7540_init();
+void ST7540_setup(struct bus_t spi_bus);
+void ST7540_get_register(struct bus_t spi_bus, uint8_t* reg);
+
+// public
+
+struct bus_t initialize();
 
 #endif /* ST7540_H_ */
